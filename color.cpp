4d0dd3f7df1ce4290a3b3753af90612adde3c8a1
ioli <bipinoli90@gmail.com>
2018-02-26 16:02:34 +0545

    Update color.cpp

--- color.cpp
+++ color.cpp
@@ -22,19 +22,6 @@ Color::~Color()
 {
 }
 
-void Color::clamp(float min, float max)
-{
-	r = std::max(min, std::min(max, r));
-	g = std::max(min, std::min(max, g));
-	b = std::max(min, std::min(max, b));
-}
-
-void Color::applyGammaCorrection(float exposure, float gamma)
-{
-	r = std::pow(r * exposure, gamma);
-	g = std::pow(g * exposure, gamma);
-	b = std::pow(b * exposure, gamma);
-}
 
 Color& Color::operator =(const Color& c)
 {
